b23233b2384173cad508382bd786a2e390d34b7a
mikeporter <porter.michael.james@gmail.com>
2017-06-20 17:54:46 -0700

    added linter

--- lab-michaelporter/lib/client.js
+++ lab-michaelporter/lib/client.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let Client = module.exports = function (socket, nick) {
+module.exports = function (socket, nick) {
   this.socket = socket;
   this.nick = nick || `guest_${Math.floor(Math.random() * 1000)}`;
 };
